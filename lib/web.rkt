7b216b47bcf4f8bf64819e068f44bb175eec3660
David K. Storrs <david.storrs@gmail.com>
2016-08-03 13:46:32 -0700

    web.rkt: contracts on all funcs, added get-page, generalized web/call

--- lib/web.rkt
+++ lib/web.rkt
@@ -3,8 +3,8 @@
 (require net/url
 		 (planet neil/html-parsing:3:0))
 
-(define (is-local? s)
-  ((or/c path-string? url?) boolean?)
+(define/contract (is-local? s)
+  (-> (or/c path-string? url?) boolean?) 
   (let ((scheme (url-scheme (cond
 							 ((url? s)  s)
 							 ((path? s) (string->url (path->string s)))
@@ -14,16 +14,34 @@
 		(equal? 'file scheme)
 		)))
 
-(define (to-url s)
+(define/contract (to-url s)
   (-> (or/c path-string? url?) url?)
   (cond ((url? s)  s)
 		((path? s) (string->url (path->string s)))
 		(else (string->url s))))
 
-(define (web/call url-string)
-  (html->xexp
-   (call/input-url (string->url url-string)
+
+(define/contract (web/call url-string
+						   #:call-proc [call-proc port->string]
+						   #:post-proc [post-proc html->xexp])
+  (->* ((or/c path-string? url?))
+	   (#:call-proc procedure? #:post-proc procedure?)
+	   any)
+  (post-proc
+   (call/input-url (to-url url-string)
 				   (curry get-pure-port #:redirections 5)
-				   port->string)))
+				   call-proc)))
+
+
+(define/contract (get-page source 
+				  #:as-text [as-text #f])
+  (->* ((or/c path-string? url?))
+	   (#:as-text boolean?)
+	   (or/c string? list?))
+  ((if as-text identity html->xexp) 
+	((if (is-local? source)
+		 (compose port->string open-input-file url->string to-url)
+		 (curry web/call #:post-proc identity))
+	 source)))
 
 (provide (all-defined-out))
