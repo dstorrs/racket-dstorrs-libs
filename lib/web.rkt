f2f95e109d06fe81011f5f1af03e03ec47b84e13
David K. Storrs <david.storrs@gmail.com>
2016-08-03 21:34:29 -0700

    web.rkt: get-page and web/call now take #:as-text and #:post-proc arguments

--- lib/web.rkt
+++ lib/web.rkt
@@ -21,27 +21,40 @@
 		(else (string->url s))))
 
 
+;;----------------------------------------------------------------------
+;;    Get a page from the internet. Accepts a path, string, or url.
+;;    'call-proc' gets passed to call/input-url.  'post-proc' gets the
+;;    result of call/input-url
+;;
 (define/contract (web/call url-string
 						   #:call-proc [call-proc port->string]
-						   #:post-proc [post-proc html->xexp])
+						   #:post-proc [post-proc html->xexp]
+						   #:as-text   [as-text #f]) ;; Really just a convenient shortcut
   (->* ((or/c path-string? url?))
-	   (#:call-proc procedure? #:post-proc procedure?)
+	   (#:call-proc (-> input-port? any)
+		#:post-proc (-> any/c any)
+		#:as-text boolean?)
 	   any)
-  (post-proc
+  ((if as-text identity post-proc)  
    (call/input-url (to-url url-string)
 				   (curry get-pure-port #:redirections 5)
-				   call-proc)))
+				   call-proc))) ;; Note that if you passed #:as-text, this better return a string
 
 
+;;--------------------------------------------------------------------------------
+;;    Get a page from the internet (via web/call) or from a
+;;    file. 'post-proc' will be run across the results.
 (define/contract (get-page source 
-				  #:as-text [as-text #f])
+				  #:post-proc [post-proc html->xexp]
+				  #:as-text   [as-text #f]) ; easier to remember than '#:post-proc identity'
   (->* ((or/c path-string? url?))
-	   (#:as-text boolean?)
+	   (#:post-proc (-> string? any/c)
+	    #:as-text boolean?)
 	   (or/c string? list?))
-  ((if as-text identity html->xexp) 
+  ((if as-text identity post-proc)
 	((if (is-local? source)
 		 (compose port->string open-input-file url->string to-url)
-		 (curry web/call #:post-proc identity))
+		 (curry web/call #:as-text #t))
 	 source)))
 
 (provide (all-defined-out))
