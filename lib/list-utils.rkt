28daf2c9ad8141479634b0bda97a672f45617e75
David K. Storrs <david.storrs@gmail.com>
2016-08-13 00:27:09 -0700

    Finalized member-rec in list-utils

--- lib/list-utils.rkt
+++ lib/list-utils.rkt
@@ -38,30 +38,32 @@
   (foldl get-once s (autobox keys)))
 
 ;;----------------------------------------------------------------------
+;;    Search through a list recursively for all instances of an item,
+;;    includes ones that are nested inside other instances.  The item
+;;    can be either a value or a predicate.  Returns a list of all
+;;    instances; nested items will appear both in their parent and on
+;;    their own.  e.g.:
+;;
+;; (define l '(1 2 (table 1) ((4) 5 (((table 2 (table 3)))))))
+;; (member-rec 2 l)                  => '(2 2)
+;; (member-rec (curry equal? 2) l)   => '(2 2)
+;; (member-rec number? l)            => '(1 2 1 4 5 2 3)
+;; (member-rec (lambda (x) (and
+;; 						 (list? x)
+;; 						 (not (null? x))
+;; 						 (equal? (car x) 'table)))
+;; 			l)  => '((table 1) (table 2 (table 3)) (table 3))
+;;
+(define/contract (member-rec m lst)
+  (-> any/c any/c list?)
+  (define match (if (procedure? m) m (curry equal? m)))
+  (define search (compose autobox (curry member-rec match)))
+  (define (recur l) (append (search (car l)) (search (cdr l))))
+  (cond
+   ((atom? lst) (if (match lst) (list lst) null))
+   ((null? lst) null)
+   ((match lst) (append (list lst) (recur lst)))
+   (else (recur lst))))
 
-;;    Search through a list for all instances of an item.  e.g.:
-;; (member-rec
-;; (define (member-rec a)
-;;   (cond
-;;    ((atom? a) null)
-;;    ((null? a) null)
-;;    ((and (list? a) (equal? (car a) 'table))
-;; 	(append (list a)
-;; 			(member-rec (car a))
-;; 			(member-rec (cdr a))))
-;;    (else (append (member-rec (car a))
-;; 				 (member-rec (cdr a))))))
-
-(define (member-rec match lst)
-  (let ((func (if (procedure? match) match (curry equal? match))))
-	(cond
-	 ((atom? lst) null)
-	 ((null? lst) null)
-	 ((func lst)
-	  (append (list lst)
-			  (member-rec func (car lst))
-			  (member-rec func (cdr lst))))
-	 (else (append (member-rec func (car lst))
-				   (member-rec func (cdr lst)))))))
 ;;----------------------------------------------------------------------
 (provide (all-defined-out))
