9dfd77e7dd62b9f2390b6232638b75b1b0097ad1
David K. Storrs <david.storrs@gmail.com>
2016-11-30 17:37:30 -0800

    added create/raise-exn to exceptions.rkt

--- dstorrs/exceptions.rkt
+++ dstorrs/exceptions.rkt
@@ -4,4 +4,8 @@
   (-> procedure? string? exn?)
   (ctor msg (current-continuation-marks)))
 
+(define/contract (create/raise-exn ctor msg)
+  (-> procedure? string? void?)
+  (raise (create-exn ctor msg)))
+                  
 (provide (all-defined-out))
