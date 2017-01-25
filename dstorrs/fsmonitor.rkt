4528e28dc863147fb7565145e005bd1506e15b9c
David K. Storrs <david.storrs@gmail.com>
2017-01-25 13:28:47 -0500

    fsmonitor added

--- dstorrs/fsmonitor.rkt
+++ dstorrs/fsmonitor.rkt
@@ -1,37 +1,105 @@
-#lang at-exp racket
+#lang at-exp rackjure
 
-(require db)
+(require db
+         dstorrs/try
+         kw-utils/partial
+         )
 
 ;;----------------------------------------------------------------------
 
-(define/contract (fsmonitor-table-schema db-type)
-  (-> (or/c 'pg 'postgres 'postgresql 'sqlite 'mysql)
+(define/contract (fsmonitor-setup-sql db-type)
+  (-> (or/c 'pg 'postgres 'postgresql 'sqlite)
       (non-empty-listof string?))
   (cond [(ormap (curry equal? db-type) '(pg postgres postgresql))
          (list
-           @~a{CREATE TABLE "watched_files" (id BIGSERIAL,path TEXT NOT NULL UNIQUE ,modified TIMESTAMP NOT NULL,size BIGINT NOT NULL,"is_dir" BOOLEAN NOT NULL DEFAULT FALSE,CONSTRAINT "watched_files_pkey" PRIMARY KEY (id))}
-           @~a{CREATE INDEX "watched_files_path_idx" ON "watched_files"(path)})
+          @~a{
+              CREATE TABLE "watched_files"
+                     (
+                      id BIGSERIAL,
+                         dir TEXT NOT NULL,
+                         filename TEXT,
+                         modified BIGINT NOT NULL,
+                         "file_size_bytes" BIGINT NOT NULL,
+                         "is_missing" BOOL NOT NULL DEFAULT false,
+                         "last_checked" BIGINT NOT NULL,
+                         CONSTRAINT "watched_files_pkey" PRIMARY KEY (id)
+                         )}
+          @~a{ALTER TABLE "watched_files" ADD CONSTRAINT "watched_files_cns_1" UNIQUE (dir,filename)}
+          @~a{CREATE INDEX "watched_files_dir_idx" ON "watched_files"(dir)}
+          )
          ]
         [(equal? db-type 'sqlite)
          (list
-          @~a{CREATE TABLE IF NOT EXISTS watched_files (id BIGINT PRIMARY KEY,path TEXT NOT NULL UNIQUE,modified TIMESTAMP NOT NULL,size BIGINT NOT NULL,is_dir BOOLEAN DEFAULT 'FALSE' NOT NULL)}
-          @~a{CREATE INDEX watched_files_path_idx ON watched_files(path)}
-          )]
-        [(equal? db-type 'mysql)
-         (list
-          @~a{CREATE TABLE IF NOT EXISTS watched_files(id BIGINT AUTO_INCREMENT,path TEXT NOT NULL UNIQUE ,modified TIMESTAMP NOT NULL,size BIGINT UNSIGNED NOT NULL,is_dir BOOLEAN NOT NULL DEFAULT FALSE,PRIMARY KEY (id))}
-          @~a{CREATE INDEX watched_files_path_idx ON watched_files (path(50))}
-          )]))
+
+          @~a{CREATE TABLE IF NOT EXISTS watched_files
+                     (
+                      id BIGINT PRIMARY KEY,
+                         dir TEXT NOT NULL,
+                         filename TEXT NOT NULL,
+                         is_dir BOOL DEFAULT 'false' NOT NULL,
+                         modified BIGINT NOT NULL,
+                         file_size_bytes BIGINT NOT NULL,
+                         is_missing BOOL DEFAULT 'false' NOT NULL,
+                         last_checked BIGINT NOT NULL
+                         )}
+         @~a{CREATE UNIQUE INDEX IF NOT EXISTS watched_files_cns_1 ON watched_files (dir,filename)}
+         @~a{CREATE INDEX watched_files_dir_idx ON watched_files(dir)}
+         )]))
 
 ;;----------------------------------------------------------------------
 
-(define/contract (watch-dir dir dbh-maker [handler identity] #:recursive [recur #t])
-  (->* (path-string? (-> connection?))
-       ((-> filesystem-change-evt? any)
-        #:recursive boolean?)
-       any)
-  (raise "not implemented")
+(define/contract (ensure-fsmonitor-table-exists db db-type)
+  (-> connection? (or/c 'pg 'postgres 'postgresql 'sqlite) any)
+
+  ;;    Test if the table exists before we do anything.  If not, set it up.
+  (try  [(query-maybe-value db "select 1 from watched_files limit 1")]
+        [catch (exn:fail?
+                (lambda (e)
+                  (call-with-transaction
+                   db
+                   (thunk
+                    (for ((stmt (fsmonitor-setup-sql db-type)))
+                      (query-exec db stmt))))
+                  ;;
+                  ;;    Verify that the table was set up, throw if not.
+                  (query-maybe-value db "select 1 from watched_files limit 1")))])
   )
 
+;;----------------------------------------------------------------------
+
+(define/contract (watch-dir dir dbh-maker handler)
+  (-> path-string?
+      (-> connection?)                  ;; dbh-maker is a thunk that returns a dbh
+      (-> path-string? connection? any) ;; proc takes dir and a dbh
+      any)
+
+  ;;    Create a watch-filesystem event for this directory
+  ;;    Put it in a thread, make the thread loop forever
+  (thread
+   (thunk
+    (do ()
+        (#f)
+      (let ((change (sync (filesystem-change-evt dir))))
+        (handler dir (dbh-maker)))
+      ))))
+
+;;----------------------------------------------------------------------
+
+(define/contract (watch-dir-tree dir dbh-maker handler #:recursive [recursive #t])
+  (->* (path-string? (-> connection?) (-> any))
+       (#:recursive boolean?)
+       any)
+
+  ;;    Watch the specified directory.  If recursive was set, walk
+  ;;    down into the directory looking for other directories, then
+  ;;    recur on them.
+  (watch-dir dir dbh-maker handler)
+  (when recursive
+    (fold-files (lambda (fpath type acc)
+                  (when (equal? 'dir type)
+                    (watch-dir-tree fpath dbh-maker handler)))
+                '()
+                dir))
+  )
 
 (provide (all-defined-out))
--- dstorrs/t/fsmonitor.t
+++ dstorrs/t/fsmonitor.t
@@ -5,7 +5,44 @@
 (require dstorrs/test-more
          dstorrs/utils
          dstorrs/fsmonitor
+         db
+         racket/runtime-path
 		 )
+
+
+
+(define-runtime-path thisdir ".")
+(define-runtime-path testdir "./data-fsmonitor-test-data")
+
+(define db-path (build-path thisdir "test_db.sqlite"))
+
+(define (new-db)
+  (when (file-exists? db-path) (delete-file db-path))
+  (sqlite3-connect db-path #:mode 'create))
+
+
+(define (refresh-test-data)
+  ;;    Delete all the files that are currently there
+  (delete-directory/files testdir)
+
+  ;;    Recreate the test files:
+  ;; <testdir>/{0-2}/{a-c}
+  (make-directory* testdir)
+
+  (define data "foo")
+  (define res '()) ;; There must be a smoother way to do this
+  (for ((i 2))
+    (define dir (build-path testdir (number->string i)))
+    (make-directory* dir)
+    (for ((name '("a" "b" "c")))
+      (define fpath (build-path dir name))
+      (display-to-file data fpath)
+      (cons (vector (path->string fpath) (length data)))))
+
+  (reverse res)
+  )
+
+;;----------------------------------------------------------------------
 
 (ok #t "testing harness works")
 
@@ -13,35 +50,68 @@
  "fs-monitor-table-schema"
 
  (define pg (list
-             @~a{CREATE TABLE "watched_files" (id BIGSERIAL,path TEXT NOT NULL UNIQUE ,modified TIMESTAMP NOT NULL,size BIGINT NOT NULL,"is_dir" BOOLEAN NOT NULL DEFAULT FALSE,CONSTRAINT "watched_files_pkey" PRIMARY KEY (id))}
+             @~a{CREATE TABLE "watched_files" (id BIGSERIAL,path TEXT NOT NULL UNIQUE ,last_modified TIMESTAMP NOT NULL,file_size_bytes BIGINT NOT NULL,"is_dir" BOOLEAN NOT NULL DEFAULT FALSE,CONSTRAINT "watched_files_pkey" PRIMARY KEY (id))}
              @~a{CREATE INDEX "watched_files_path_idx" ON "watched_files"(path)}
              ))
  (define sqlite (list
-                 @~a{CREATE TABLE IF NOT EXISTS watched_files (id BIGINT PRIMARY KEY,path TEXT NOT NULL UNIQUE,modified TIMESTAMP NOT NULL,size BIGINT NOT NULL,is_dir BOOLEAN DEFAULT 'FALSE' NOT NULL)}
+                 @~a{CREATE TABLE IF NOT EXISTS watched_files (id BIGINT PRIMARY KEY,path TEXT NOT NULL UNIQUE,last_modified TIMESTAMP NOT NULL,file_size_bytes BIGINT NOT NULL,is_dir BOOLEAN DEFAULT 'FALSE' NOT NULL)}
                  @~a{CREATE INDEX watched_files_path_idx ON watched_files(path)}
                  ))
- (define mysql (list
-                @~a{CREATE TABLE IF NOT EXISTS watched_files(id BIGINT AUTO_INCREMENT,path TEXT NOT NULL UNIQUE ,modified TIMESTAMP NOT NULL,size BIGINT UNSIGNED NOT NULL,is_dir BOOLEAN NOT NULL DEFAULT FALSE,PRIMARY KEY (id))}
-                @~a{CREATE INDEX watched_files_path_idx ON watched_files (path(50))}
-                ))
  (for ((name '(pg postgres postgresql)))
-   (is (fsmonitor-table-schema name)
+   (is (fsmonitor-setup-sql name)
        pg
        (~a "Got the proper schema for " name)))
 
- (is (fsmonitor-table-schema 'sqlite)
+ (is (fsmonitor-setup-sql 'sqlite)
      sqlite
      "Got the proper schema for sqlite")
-
- (is (fsmonitor-table-schema 'mysql)
-     mysql
-     "Got the proper schema for mysql")
-
  ); test-suite
 
+;;----------------------------------------------------------------------
+
 (test-suite
- "watch-dir, no recur"
- (ok #f "not done")
+ "take-directory-snapshot"
+
+ (define db (new-db))
+ (refresh-test-data)
+
+ (let ((numrows (query-rows db "select count(1) from watched_files")))
+   (is numrows 0  "started off with no rows"))
+
+ (define snapshot (take-directory-snapshot testdir))
+
+ (define base (path->string testdir))
+
+ (let ((rows (query-rows db "select path, file_size_bytes from watched_files order by path")))
+   (is rows
+       snapshot
+       "take-directory-snapshot did the right thing"))
  )
+
+;;----------------------------------------------------------------------
+
+(test-suite
+ "compare-directory"
+
+ (ok #f "not implemented")
+ )
+
+;;----------------------------------------------------------------------
+
+(test-suite
+ "watch-dir, not recursive"
+
+ (ok #f "not implemented")
+ )
+
+;;----------------------------------------------------------------------
+
+(test-suite
+ "watch-dir recursive"
+
+ (ok #f "not implemented")
+ )
+
+
 
 (displayln "Done testing.")
