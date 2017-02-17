1b7cd00a6e026d4a6328b062e03528fd6d9f8055
David K. Storrs <david.storrs@gmail.com>
2017-02-17 14:23:46 -0500

    create-exn and create/raise-exn now take rest arguments

--- dstorrs/exceptions.rkt
+++ dstorrs/exceptions.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(define/contract (create-exn ctor msg [args '()])
-  (->* (procedure? string?) (listof any/c) exn?)
-  (apply ctor (append (list msg (current-continuation-marks)) args))
+(define/contract (create-exn ctor msg . args)
+  (->* (procedure? string?) () #:rest (listof any/c) exn?)
+  (apply ctor (append (list msg (current-continuation-marks)) args)))
 
-(define/contract (create/raise-exn ctor msg [args '()])
-  (->* (procedure? string?) (listof any/c) any)
+(define/contract (create/raise-exn ctor msg . args)
+  (->* (procedure? string?) () #:rest (listof any/c) any)
   (raise (create-exn ctor msg args)))
                   
 (provide (all-defined-out))
