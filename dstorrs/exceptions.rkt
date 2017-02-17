8286e9b47f5d9b1722c4dbeafd3f7e5a19335c68
David K. Storrs <david.storrs@gmail.com>
2017-02-17 14:06:18 -0500

    dstorrs/exceptions: create-exn and create/raise-exn now accept an args list

--- dstorrs/exceptions.rkt
+++ dstorrs/exceptions.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(define/contract (create-exn ctor msg)
-  (-> procedure? string? exn?)
-  (ctor msg (current-continuation-marks)))
+(define/contract (create-exn ctor msg [args '()])
+  (->* (procedure? string?) (listof any/c) exn?)
+  (apply ctor (append (list msg (current-continuation-marks)) args))
 
-(define/contract (create/raise-exn ctor msg)
-  (-> procedure? string? void?)
-  (raise (create-exn ctor msg)))
+(define/contract (create/raise-exn ctor msg [args '()])
+  (->* (procedure? string?) (listof any/c) any)
+  (raise (create-exn ctor msg args)))
                   
 (provide (all-defined-out))
