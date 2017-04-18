e01a2c0e9a59b1c818447ed5b09b976608a6a33e
David K. Storrs <david.storrs@gmail.com>
2017-04-18 10:24:33 -0400

    db.rkt: The various query-rows-as-dicts functions now take rest arguments so you don't need to pass bind values as a list

--- dstorrs/db.rkt
+++ dstorrs/db.rkt
@@ -35,44 +35,84 @@
 
 ;;----------------------------------------------------------------------
 
-;;    Run a query against the database, return the result as a list of
-;;    dicts. (By default hashes, but you can override if desired.)
-;;    You can also provide a transformer function that will be given
-;;    the dict for processing before it is returned.  It must accept a
-;;    dict? and return a dict/
+;;    Run a query against the database, return the result as a
+;;    list of dicts. (By default hashes, but you can override
+;;    if desired.)  You can also provide transformer functions
+;;    to rewrite the data before the hashes are built or
+;;    rewrite the results before they are returned.
 ;;
-;; (query-as-dicts '(chunk-hash chunk-num) (thunk (query-rows db "select hash, chunk_num from chunks")))
-;; (query-as-dicts '(chunk-hash chunk-num) (thunk (query-rows db "select hash, chunk_num from chunks")) #:dict-maker hasheq)
-;; (query-as-dicts '(chunk-hash chunk-num)
-;;                 (thunk (query-rows db "select hash, chunk_num from chunks"))
-;;                 #:transform (lambda (d) (hash-set d 'processed #t))) ;; add a key
-(define/contract (query-rows-as-dicts keys
-                                      db
-                                      sql
-                                      [vals '()]
-                                      #:dict-maker [dict-maker make-hash]
+;;
+;; ;Run a query that needs no params. Result is a list of
+;; ;mutable hashes matching (hash/c symbol? any/c)
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks")
+;;
+;; ;Run a query with params    
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1")
+;;                      7)
+;;
+;; ;Run a query but return the results as a list of 2-item lists
+;; ;instead of a list of hashes.  
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1")
+;;                      7
+;;                      #:dict-maker flatten ;; e.g. '(foo . 7) => '(foo 7)
+;;
+;; ;Run a query with params, add one to each chunk-num before
+;; ;returning the results.
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1")
+;;                      7
+;;                      #:transform-data (lambda (k v) (cons k (add1 v)))
+;;
+;; ;Run a query with params, convert the keys from symbols to
+;; ;strings before returning the results.
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1")
+;;                      7
+;;                      #:transform-dict (lambda (d)
+;;                                         (for/hash ((k (hash-keys d)))
+;;                                           (values (symbol->string k) (hash-ref d k))) 
+(define/contract (query-rows-as-dicts keys db sql
+                                      #:dict-maker     [dict-maker make-hash]
                                       #:transform-dict [transform-dict identity]
-                                      #:transform-data [transform-data cons])
-  (->* ((non-empty-listof any/c) connection? string?)
-       (list?
-        #:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
-        #:transform-data (-> any/c any/c pair?)  ; transform the input of dict-maker
-        #:transform-dict (-> dict? dict?)        ; transform the output of dict-maker
+                                      #:transform-data [transform-data cons]
+                                      .
+                                      params)
+  (->* (list? connection? string?)
+       (
+        #:dict-maker procedure?
+        #:transform-dict procedure?
+        #:transform-data procedure?
         )
-       (listof dict?))
-  ;; (define v->d (curry vector->dict
-  ;;                     keys
-  ;;                     #:dict-maker dict-maker
-  ;;                     #:transform-data transform-data
-  ;;                     #:transform-dict transform-dict))
+       #:rest list?
+       list?)
+
+       ;;  #:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
+       ;;  #:transform-data (-> any/c any/c pair?)  ; transform the input of dict-maker
+       ;;  #:transform-dict (-> dict? dict?)        ; transform the output of dict-maker
+       ;;  )
+       ;; #:rest (listof any/c)
+       ;; (listof dict?))
+
+  (define vals (flatten params))
   (define (v->d v)
     (vector->dict
      keys
      v
-     #:dict-maker dict-maker
+     #:dict-maker     dict-maker
      #:transform-data transform-data
      #:transform-dict transform-dict)
     )
+  (say "get-rows-as-dicts, vals: " vals)
+  (say "db: " db)
+  (say "sql: " sql)
   (map  v->d
         (if (null? vals)
             (query-rows db sql)
@@ -83,41 +123,53 @@
 (define/contract (query-row-as-dict keys
                                     db
                                     sql
-                                    [vals '()]
                                     #:dict-maker [dict-maker make-hash]
                                     #:transform-dict [transform-dict identity]
-                                    #:transform-data [transform-data cons])
+                                    #:transform-data [transform-data cons]
+                                    .
+                                    vals
+                                    )
   (->* ((non-empty-listof any/c) connection? string?)
        (list?
         #:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
         #:transform-data (-> any/c any/c pair?)  ; transform the input of dict-maker
         #:transform-dict (-> dict? dict?)        ; transform the output of dict-maker
         )
+       #:rest (listof any/c)
        dict?)
 
-  (car (query-rows-as-dicts keys db sql vals 
-                            #:dict-maker     dict-maker
-                            #:transform-dict transform-dict
-                            #:transform-data transform-data
-                            )))
+  (define res (query-rows-as-dicts keys db sql vals
+                                   #:dict-maker     dict-maker
+                                   #:transform-dict transform-dict
+                                   #:transform-data transform-data
+                                   ))
+  (if (null? res)
+      (dict-maker)
+      (car res)))
 
 ;;----------------------------------------------------------------------
 
 (define/contract (query-maybe-row-as-dict keys
                                           db
                                           sql
-                                          [vals '()]
-                                          #:dict-maker [dict-maker make-hash]
+                                          #:dict-maker     [dict-maker make-hash]
                                           #:transform-dict [transform-dict identity]
-                                          #:transform-data [transform-data cons])
+                                          #:transform-data [transform-data cons]
+                                          .
+                                          args
+                                          )
   (->* ((non-empty-listof any/c) connection? string?)
-       (list?
-        #:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
+       (#:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
         #:transform-data (-> any/c any/c pair?)  ; transform the input of dict-maker
         #:transform-dict (-> dict? dict?)        ; transform the output of dict-maker
         )
+       #:rest (listof any/c)
        dict?)
 
+  ;;    If they passed the arguments as a list it will be received
+  ;;    with an extra list wrapper, e.g. '(("foo")). Unwrap it to
+  ;;    '("foo")
+  (define vals (flatten args))
   (define res
     (if (null? vals)
         (query-maybe-row db sql)
