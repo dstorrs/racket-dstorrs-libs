09ff11ec3551c13ebd4469feb69b1bc38837f79a
David K. Storrs <david.storrs@biomantica.com>
2017-12-27 12:24:56 -0500

    HTML-Element.rkt : html-element-from is now more accepting of sources. added xexp? and made it handle non-lists

--- dstorrs/HTML-Element.rkt
+++ dstorrs/HTML-Element.rkt
@@ -1,42 +1,61 @@
 #lang racket
 
 (require net/url
-		 net/url-connect
-		 openssl
-		 rackunit
-		 racket/pretty
-		 "list-utils.rkt" ; for atom?
-		 "web.rkt"        ; for web/call
-		 (planet neil/html-parsing:3:0)
-		 sxml
-		 )
+         net/url-connect
+         openssl
+         rackunit
+         racket/pretty
+         "list-utils.rkt" ; for atom?
+         "web.rkt"        ; for web/call
+         "try.rkt"
+         (planet neil/html-parsing:3:0)
+         sxml
+         dstorrs/utils
+         )
 
 (define (xexp? x)
-  (-> list? boolean?)
-  (equal? (first x) '*TOP*))
-  
+  (-> any/c boolean?)
+  (and (list? x) (not-null? x) (equal? (first x) '*TOP*)))
+
 (define attr-list? (ntype?? '@))
 
 ;;--------------------------------------------------
-;;    attr-val:  xexp 'key [default value]  -> any
-(define (attr-val x key [def #f])
-  (let ((h (attr-hash x)))
-	(if (hash? h)
-		(hash-ref h key def)
-		def)))
+;;    attr-val:  xexp 'key [default value]  -> any/c
+(define/contract (attr-val x key [def #f])
+  (->* (xexp? symbol?) (any/c) any/c)
+
+  (hash-ref (attr-hash x) key def))
 
 ;;--------------------------------------------------
 ;;    (html-element-from src) -> xexpr
-;;        source : string (URL, HTML, or filepath)
+;;        source : input-port?, path-string?, url-string?, or string
+;;
+;;  If given a string source it will try to work out what the thing is
+;;  -- a path to an HTML file, a URL to a web page, or some actual
+;;  HTML.
 ;;
 (define (html-element-from src)
-  (let ([is-url  #px"^https?://"]
-		[is-html #px"^<(!DOCTYPE|[A-Za-z]+)"])
-	(cond [(regexp-match is-url  src) (web/call src)]
-		  [(regexp-match is-html src) (html->xexp (open-input-string src))]
-		  [else (let ((p (open-input-file src)))
-				   (begin0 (html->xexp p)
-						   (close-input-port p)))])))
+  (-> (or/c url? path? input-port? string? xexp?) xexp?)
+
+  (define is-url  #px"^https?://")
+  (define is-html #px"^<(!DOCTYPE|[A-Za-z]+)")
+
+  (define (from-file)
+    (when (not (file-exists? src))
+      (raise-arguments-error 'html-element-from "argument looks like path but not to existing file" "arg" src))
+    
+    (define p (open-input-file src))
+    (try [(html->xexp p)]
+         [catch (match-anything raise)]
+         [finally (close-input-port p)]))
+  
+  (cond [(xexp? src)  src]
+        [(port? src)  (html->xexp src)]
+        [(path? src)  (from-file)]
+        [(url?  src)  (web/call src)]
+        [(regexp-match is-url  src) (web/call src)]
+        [(regexp-match is-html src)  (html->xexp (open-input-string src))]
+        [else  (from-file)]))
 
 ;;--------------------------------------------------
 ;;    Take an xexp, a hash, and a quoted symbol (e.g. 'class) and
@@ -46,13 +65,13 @@
 (define (has-attr? html-expr key [val #f])
   (define h (attr-hash html-expr))
   (define v (and (hash-has-key? h key)
-				 (hash-ref h key)))
+                 (hash-ref h key)))
   (if val
-	  (and (regexp-match
-			(pregexp (string-append "\\b" val "\\b"))
-			val)
-		   v)
-	  v))
+      (and (regexp-match
+            (pregexp (string-append "\\b" val "\\b"))
+            val)
+           v)
+      v))
 
 
 
@@ -70,13 +89,13 @@
 (define (attr-hash el)
   ;;(displayln (format "### In attr-hash, el is: ~a" el))
   (define (attr-hash-helper l)
-	(cond [(atom? l)  (hash)]
-		  [(null? l)  (hash)]
-		  [(atom? (car l))  (hash)]
-		  [(< (length (car l)) 2)  (hash)]
-		  [else (hash-set (attr-hash-helper (cdr l))
-						  (first (car l))
-						  (string-trim (second (car l))))]))
+    (cond [(atom? l)  (hash)]
+          [(null? l)  (hash)]
+          [(atom? (car l))  (hash)]
+          [(< (length (car l)) 2)  (hash)]
+          [else (hash-set (attr-hash-helper (cdr l))
+                          (first (car l))
+                          (string-trim (second (car l))))]))
   (attr-hash-helper (sxml:attr-list el)))
 
 
@@ -87,13 +106,13 @@
 (define (text-of x)
   (string-trim
    (cond [(string? x) (string-trim x)]
-		 [(atom? x)   ""]
-		 [(empty? x)  ""]
-		 [((ntype?? 'br) x)  "\n"]
-		 [(attr-list? x)  ""]
-		 [else
-		  (string-join (list (text-of (car x))
-							 (text-of (cdr x))))])
+         [(atom? x)   ""]
+         [(empty? x)  ""]
+         [((ntype?? 'br) x)  "\n"]
+         [(attr-list? x)  ""]
+         [else
+          (string-join (list (text-of (car x))
+                             (text-of (cdr x))))])
    #px"[\t ]+" ;; don't trim newlines
    ))
 ;;  (p (@ (class "hi") (style "border: 1px solid red;")) "Hello world")
@@ -105,8 +124,8 @@
 ;;    structs in Scribble.
 (define (hlink-data hlink-xexp [base ""])
   (list (->absolute-url base
-						(hash-ref (attr-hash hlink-xexp) 'href)) ;; URL
-		(get hlink-xexp '(2))))               ;; link text
+                        (hash-ref (attr-hash hlink-xexp) 'href)) ;; URL
+        (get hlink-xexp '(2))))               ;; link text
 
 ;;----------------------------------------------------------------------
 ;;    Take an 'A' xexp and return the URL as a string
@@ -127,7 +146,7 @@
 ;;    Get the xexp for the first link in a document
 (define (first-hlink x)
   (let ((r (all-hlinks x)))
-	(if (null? r) r (first r))))
+    (if (null? r) r (first r))))
 
 ;;----------------------------------------------------------------------
 ;;    look-down  ->  returns list or string
@@ -153,59 +172,59 @@
 ;;    must match exactly.  If it is a regexp or pregexp then the match
 ;;    must succeed.
 (define (look-down some-content
-				   [action list]             ;; Default: list all matches
-				   #:match [:match-func #f]  ;; Default: match everything
-				   #:tag   [:tag #f]         ;; Ditto
-				   #:attr  [:attr #f]        ;; Ditto
-				   )
+                   [action list]             ;; Default: list all matches
+                   #:match [:match-func #f]  ;; Default: match everything
+                   #:tag   [:tag #f]         ;; Ditto
+                   #:attr  [:attr #f]        ;; Ditto
+                   )
   (define (match-all x) #t)
   (define :tag? (if :tag (ntype?? :tag) match-all))
   (define :match? (if :match-func :match-func match-all))
   (define h (attr-hash some-content))
   (define (coerce-to-regexp val)
-	(if (string? val)
-		(pregexp (string-append "\\b" val "\\b"))
-		val))
+    (if (string? val)
+        (pregexp (string-append "\\b" val "\\b"))
+        val))
   (define (verify-attr el)
-	(cond
-	 [(pair? :attr)            ;; car will be key, cdr is string or regexp
-	  (let* ([key (car :attr)]
-			 [val (cdr :attr)]
-			 [res   (has-attr? el key)])
-		;;(displayln  "verify attr, regex match (el, k, v, res): ~a,~a,~a,~a" el key val res)
-		(and res
-			 (regexp-match (coerce-to-regexp val) res)))]
-	 ;;
-	 ;;    :attr can be just a name; the attribute must exist in the element
-	 [:attr (has-attr? some-content :attr)]
-	 ;;
-	 ;; if :attr wasn't set, then validation automatically succeeds
-	 [else #t]))
+    (cond
+      [(pair? :attr)            ;; car will be key, cdr is string or regexp
+       (let* ([key (car :attr)]
+              [val (cdr :attr)]
+              [res   (has-attr? el key)])
+         ;;(displayln  "verify attr, regex match (el, k, v, res): ~a,~a,~a,~a" el key val res)
+         (and res
+              (regexp-match (coerce-to-regexp val) res)))]
+      ;;
+      ;;    :attr can be just a name; the attribute must exist in the element
+      [:attr (has-attr? some-content :attr)]
+      ;;
+      ;; if :attr wasn't set, then validation automatically succeeds
+      [else #t]))
   (define (searched-for? el)
-	;;(displayln (format "### in searched for, (tag? el) '~a', el: ~a" (:tag? el) el))
-	(and (:tag? el)  ;; Use 'and' instead of 'or' so that :match? can trump
-		 (verify-attr el)
-		 (:match? el)))
+    ;;(displayln (format "### in searched for, (tag? el) '~a', el: ~a" (:tag? el) el))
+    (and (:tag? el)  ;; Use 'and' instead of 'or' so that :match? can trump
+         (verify-attr el)
+         (:match? el)))
   (define (ld c) ;; can't use curryr here since we need to use keyword args
-	;;(displayln (format "ld. attr: '~a'. atom? c: ~a " :attr (atom? c)))
-	(autobox
-	 (if (atom? c)
-		 empty
-		 (look-down c action #:match searched-for? #:attr :attr))))
+    ;;(displayln (format "ld. attr: '~a'. atom? c: ~a " :attr (atom? c)))
+    (autobox
+     (if (atom? c)
+         empty
+         (look-down c action #:match searched-for? #:attr :attr))))
   ;;
   ;; NOTE: Body of 'look-down' starts here
   (cond
-   [(empty?        some-content)  (begin
-									;;(displayln (format  "### empty some-content"))
-									empty)]
-   [(attr-list?    some-content)  (begin
-									;;(displayln (format  "### is attr list"))
-									empty)]
-   [(searched-for? some-content)  (begin
-									;;(displayln (format  "### cond searched-for some-content"))
-									(autobox (action some-content)))]
-   [else                          (begin ;;(displayln (format "### else"))
-										 (apply append (map ld some-content)))]))
+    [(empty?        some-content)  (begin
+                                     ;;(displayln (format  "### empty some-content"))
+                                     empty)]
+    [(attr-list?    some-content)  (begin
+                                     ;;(displayln (format  "### is attr list"))
+                                     empty)]
+    [(searched-for? some-content)  (begin
+                                     ;;(displayln (format  "### cond searched-for some-content"))
+                                     (autobox (action some-content)))]
+    [else                          (begin ;;(displayln (format "### else"))
+                                     (apply append (map ld some-content)))]))
 
 
 ;;----------------------------------------------------------------------
