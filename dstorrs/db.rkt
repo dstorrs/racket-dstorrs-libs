e4008d68e7f448e8fc0daafc5ab9db596b5c7e3a
David K. Storrs <david.storrs@biomantica.com>
2018-01-15 12:43:35 -0500

    fixed tests on db.rkt, added comments

--- dstorrs/db.rkt
+++ dstorrs/db.rkt
@@ -9,24 +9,27 @@
          )
 
 
-;----------------------------------------------------------------------
+;======================================================================
 ;  A collection of convenience functions for working with the DB.
-;  Also re-exports everything from the racket 'db' module except for
+;
+;  Re-exports everything from the dstorrs/sql library
+;
+;  Re-exports everything from the racket 'db' module except for
 ;  the 'disconnect' function.  That one is exported as 'db:disconnect'
 ;  so as not to clash with the disconnect method from the HTTP
 ;  libraries.
-;----------------------------------------------------------------------
+;======================================================================
 
 
 
-;----------------------------------------------------------------------
 ;  First, let's define some DB-specific exceptions
 ;
 ; Base of DB exn hierarchy
 (struct exn:fail:db exn:fail () #:transparent)
 
-; DB couldn't be created
-(struct exn:fail:db:create exn:fail:db () #:transparent)
+; issues related to the DB itself
+(struct exn:fail:db:create exn:fail:db () #:transparent) ; tried to create it, couldn't
+
 
 ; issues related to a single row; doesn't exist (when it should), does
 ; exist (when it shouldn't)
@@ -40,6 +43,16 @@
 (struct exn:fail:db:num-rows exn:fail:db (expected got) #:transparent)
 (struct exn:fail:db:num-rows:zero exn:fail:db:num-rows () #:transparent)
 (struct exn:fail:db:num-rows:many exn:fail:db:num-rows () #:transparent)
+
+
+;;----------------------------------------------------------------------
+
+(define/contract (num-rows db table-name)
+  (-> connection? (or/c symbol? string?) exact-integer?)
+  (query-value db (~a "select count(1) from " table-name)))
+  
+;;----------------------------------------------------------------------
+
 
 ; refine-db-exn
 ;
@@ -78,7 +91,6 @@
                      exn:fail:db:num-rows:many
                      )
                  msg (num expected) (num got))]
-
     [_ e])
   )
 
@@ -86,70 +98,33 @@
 
 ;; Function:  query-rows-as-dicts
 ;;
+;; Summary:
+;;
 ;;    Run a query against the database, return the result as a list of
 ;;    dicts. (By default hashes, but you can override to use some
 ;;    other form of dict if desired.)  You can also provide
 ;;    transformer functions to rewrite the data before the hashes are
-;;    built or rewrite the results after they are built but before
+;;    built or rewrite the hashes after they are built but before
 ;;    they are returned.
 ;;
-;;
-;;    Run a query that needs no params. Result is a list of
-;;    mutable hashes matching (hash/c symbol? any/c)  Note that
-;;    the keys don't have to be the same as the field names
-;;
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks")
+;;    By default, if an exception is thrown from the query then an
+;;    empty dict is returned.  If you want the exn to be re-raised
+;;    then you can pass #:trap-exns #t.  In that case the exn will be
+;;    sent to 'refine-db-exn' (see above) before being re-raised.
 ;;
 ;;
-;;    Run a query with params
+;; Contract:
 ;;
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks where id = $1"
-;;                      7)
-;;
-;;
-;;    Same as the previous, except the arg is passed as a list.
-;;    (i.e., it doesn't matter if you pass the args individually or
-;;    in a list, so you can do whichever is more convenient)
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks where id = $1"
-;;                      '(7))
-;;
-;;
-;;    Run a query but return the results as a list of 2-item lists
-;;    instead of a list of hashes.
-;;
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks where id = $1"
-;;                      7
-;;                      #:dict-maker flatten) ;; e.g. '(foo . 7) => '(foo 7)
-;;
-;;
-;;    Run a query with params, add one to each chunk-num before
-;;    returning the results.
-;;
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks where id = $1"
-;;                      7
-;;                      #:transform-data (lambda (k v) (cons k (add1 v))))
-;;
-;;
-;;    Run a query with params, convert the keys of the resulting
-;;    hashes from symbols to strings before returning the results.
-;;    (hash-keys->strings is defined in dstorrs/utils)
-;;
-;; (query-rows-as-dicts '(chunk-hash chunk-num)
-;;                      db-handle
-;;                      "select hash, chunk_num from chunks where id = $1"
-;;                      7
-;;                      #:transform-dict hash-keys->strings)
-;;
+;;  (->* (list? connection? string?) ; keys, db handle, SQL string
+;;       (
+;;        #:wrapper          (-> connection (-> any) any) ; e.g. call-with-transaction/disconnect
+;;        #:trap-exns?       boolean?    
+;;        #:dict-maker       procedure?  
+;;        #:transform-dict   procedure?  
+;;        #:transform-data   procedure?  
+;;        )
+;;       #:rest list?
+;;       (listof dict?))
 ;;
 ;;
 ;; The various procedures passed through the keywords are expected
@@ -157,15 +132,87 @@
 ;; checked because raw lambdas and curried functions have no
 ;; available contract and so would fail the check.
 ;;
-;;  #:dict-maker     (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
-;;  #:transform-data (-> any/c any/c pair?)      ; transform the input of dict-maker
-;;  #:transform-dict (-> dict? dict?)            ; transform the output of dict-maker
+;;  #:wrapper        (-> (-> any) any)         ; takes a thunk, return value(s) not checked
+;;  #:trap-exns?     boolean?                  ; return empty dict instead of throw? default: #t
+;;  #:dict-maker     (-> (listof pair?) dict?) ; takes an assoc list, returns a dict
+;;  #:transform-data (-> any/c any/c pair?)    ; transform the input of dict-maker
+;;  #:transform-dict (-> dict? dict?)          ; transform the output of dict-maker
 ;;
 ;; The default dict-maker is make-hash, which produces a mutable
 ;; hash.  If you want an immutable hash then you can pass
 ;; make-immutable-hash to the #:dict-maker argument
 ;;
+;;
+;; Examples:
+;;
+;;    ;Run a query that needs no params. Result is a list of
+;;    ;mutable hashes matching (hash/c symbol? any/c)  Note that
+;;    ;the keys don't have to be the same as the field names
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks")
+;;
+;;
+;;    ;Run a query with params:
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      7)
+;;
+;;
+;;    ;Same as the previous, except the arg is passed as a list.
+;;    ;(i.e., it doesn't matter if you pass the args individually or
+;;    ;in a list, so you can do whichever is more convenient)
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      '(7))
+;;
+;;
+;;    ;Run a query but return the results as a list of 2-item lists
+;;    ;instead of a list of hashes.
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      7
+;;                      #:dict-maker flatten) ;; e.g. '(foo . 7) => '(foo 7)
+;;
+;;
+;;    ;Run a query with params, add one to each chunk-num before
+;;    ;returning the results.
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      7
+;;                      #:transform-data (lambda (k v) (cons k (add1 v))))
+;;
+;;
+;;    ;Run a query with params, convert the keys of the resulting
+;;    ;hashes from symbols to strings before returning the results.
+;;    ;(hash-keys->strings is defined in dstorrs/utils)
+;;    ;
+;;    ;NOTE: Obviously, this is contrived.  An easier solution would
+;;    ;be to make the keys be strings in the first place.  That might
+;;    ;not be straightforward, however, if the keys were being
+;;    ;auto-generated somewhere else.
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      7
+;;                      #:transform-dict hash-keys->strings)
+;;
+;;    ;Same as above, but run the query inside a transaction and then
+;;    ;disconnect the db handle
+;; (query-rows-as-dicts '(chunk-hash chunk-num)
+;;                      db-handle
+;;                      "select hash, chunk_num from chunks where id = $1"
+;;                      7
+;;                      #:transform-dict hash-keys->strings
+;;                      #:wrapper call-with-transaction/disconnect)
+;;
 (define/contract (query-rows-as-dicts keys db sql
+                                      #:wrapper        [wrapper    (lambda (db thnk) (thnk))]
+                                      #:trap-exns?     [trap-exns? #t]
                                       #:dict-maker     [dict-maker make-hash]
                                       #:transform-dict [transform-dict identity]
                                       #:transform-data [transform-data cons]
@@ -173,14 +220,16 @@
                                       params)
   (->* (list? connection? string?)
        (
-        #:dict-maker procedure?
-        #:transform-dict procedure?
-        #:transform-data procedure?
+        #:wrapper          (-> connection? (-> any) any) 
+        #:trap-exns?       boolean?
+        #:dict-maker       procedure?
+        #:transform-dict   procedure?
+        #:transform-data   procedure?
         )
        #:rest list?
        (listof dict?))
 
-  
+
   ; We flatten the parameters list as a convenience to the caller.
   ; That way you can do things like pass some arguments on their own
   ; and some as the result of map calls without having to ensure that
@@ -194,10 +243,18 @@
      #:transform-data transform-data
      #:transform-dict transform-dict)
     )
-  (map  v->d
-        (if (null? vals)
-            (query-rows db sql)
-            (apply (curry query-rows db sql) vals))))
+  (try [
+        (map  v->d
+              (wrapper
+               db
+               (thunk
+                (if (null? vals)
+                    (query-rows db sql)
+                    (apply (curry query-rows db sql) vals)))))
+        ]
+       [catch (match-anything
+               (cond [trap-exns? (dict-maker)]
+                     [else (compose1 raise refine-db-exn)]))]))
 
 ;;----------------------------------------------------------------------
 
@@ -207,19 +264,22 @@
 ; one row, so it returns a dict instead of a list of dicts.  If
 ; query-row throws an exception (probably because there was no such
 ; row) then query-row-as-dict returns an empty dict, unless you call
-; it with #:throw-on-exn? #t, in which case it throws an appropriate
+; it with #:trap-exns? #t, in which case it throws an appropriate
 ; exn:fail:db:*
 (define/contract (query-row-as-dict keys
                                     db
                                     sql
+                                    #:trap-exns? [trap-exns? #f]
                                     #:dict-maker [dict-maker make-hash]
                                     #:transform-dict [transform-dict identity]
                                     #:transform-data [transform-data cons]
                                     .
-                                    vals
+                                    params
                                     )
   (->* ((non-empty-listof any/c) connection? string?)
-       (#:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
+       (
+        #:trap-exns? boolean?
+        #:dict-maker (-> (listof pair?) dict?)   ; takes an assoc list, returns a dict
         #:transform-data (-> any/c any/c pair?)  ; transform the input of dict-maker
         #:transform-dict (-> dict? dict?)        ; transform the output of dict-maker
         )
@@ -227,20 +287,36 @@
        dict?)
 
   (define res (query-rows-as-dicts keys db sql vals
+                                   #:trap-exns?     trap-exns? 
                                    #:dict-maker     dict-maker
                                    #:transform-dict transform-dict
                                    #:transform-data transform-data
                                    ))
-  (if (null? res)
-      (dict-maker)
-      (car res)))
+  (define vals (flatten params))
+  (define (v->d v)
+    (vector->dict  ; defined in dstorrs/list-utils
+     keys
+     v
+     #:dict-maker     dict-maker
+     #:transform-data transform-data
+     #:transform-dict transform-dict)
+    )
+
+  (try [
+        (v->d keys (apply (curry query-row db sql) vals))
+        ]
+       [catch (match-anything
+               (lambda (e)
+                 (cond [trap-exns? (dict-maker)]
+                       [else (refine-db-exn e)])))]))
 
 ;;----------------------------------------------------------------------
 
 ; query-maybe-row-as-dict keys
 ;
-; Same as query-rows-as-dicts, but expects that there will be zero or one rows.
-; one row so it returns a dict instead of a list of dicts
+; Same as query-rows-as-dicts, but expects that there will be zero or
+; one rows.  one row so it returns a dict instead of a list of dicts.
+; If there is no such row then it returns an empty dict.
 (define/contract (query-maybe-row-as-dict keys
                                           db
                                           sql
@@ -294,31 +370,47 @@
 
 ;;----------------------------------------------------------------------
 
-;(define/contract (call-with-transaction/disconnect db thnk)
+; (define/contract (ensure-disconnect db thnk [wrapper unwrap-val])
 ;
-; Calls the specified thunk inside a transaction.  Exceptions will be
-; trapped and sent to refine-db-exn before being re-raised.
-; Regardless of whether there is an exception, the db handle is
-; guaranteed to be closed after this function completes.
-(define/contract (call-with-transaction/disconnect db thnk)
-  (-> connection? (-> any) any)
+; Calls the specified thunk.  Exceptions are trapped, sent to
+; refine-db-exn, and re-raised.  Regardless of whether there is an
+; exception, the db handle is guaranteed to be closed after this
+; function completes.
+;
+; Args are:  database connection, thunk, optionally a function of two arguments
+;
+; NOTE:  See call-with-transaction/disconnect below.
+;
+(define/contract (ensure-disconnect db thnk [wrapper (lambda (db thnk) (unwrap-val thnk))])
+  (->* (connection? (-> any)) ((-> connection? (-> any))) any)
 
-  (try [(call-with-transaction db thnk)]
+  (try [(wrapper db thnk)]
        [catch (match-anything (lambda (e) (raise (refine-db-exn e))))]
        [finally
         ;(say "before disconnecting.  DB is connected?: " (connected? db))
         (disconnect db)
         ;(say "after disconnecting.  DB is connected?: " (connected? db))
         ]))
+;;----------------------------------------------------------------------
+
+;(define/contract (call-with-transaction/disconnect db thnk)
+;
+; Shorthand for: (ensure-disconnect db thnk call-with-transaction))
+(define/contract (call-with-transaction/disconnect db thnk)
+  (-> connection? (-> any) any)
+
+  (ensure-disconnect db thnk call-with-transaction))
 
 ;;----------------------------------------------------------------------
 
 
-(provide refine-db-exn
+(provide num-rows
+         refine-db-exn
          query-rows-as-dicts
          query-row-as-dict
          query-maybe-row-as-dict
          query-flat
+         ensure-disconnect
          call-with-transaction/disconnect
          (except-out (all-from-out db) disconnect)
          (prefix-out db: disconnect)
