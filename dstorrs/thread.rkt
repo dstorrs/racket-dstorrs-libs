fca706460915be4c6f4f70e2c3d8a076fc02e424
David K. Storrs <david.storrs@biomantica.com>
2018-01-19 12:12:15 -0500

    thread: in 'threaded', changed the order so that the existing prefix-for-say is prepended to the thread label instead of the other way around.  Also, comments.

--- dstorrs/thread.rkt
+++ dstorrs/thread.rkt
@@ -6,8 +6,14 @@
          threaded
          )
 
-;;----------------------------------------------------------------------
+;;======================================================================
+;;    Functions for conveniently threading your code.
+;;======================================================================
 
+;  (define/contract (execute-thunk thnk  #:async? [async? #f])
+;
+; Run a thunk.  If you set #:async? #t then it will be run in another
+; thread via 'threaded' (see below)
 (define/contract (execute-thunk thnk  #:async? [async? #f])
   (->* ((-> any)) ( #:async? boolean?) any)
   (if async?
@@ -16,9 +22,14 @@
 
 ;;----------------------------------------------------------------------
 
+; (define/contract (threaded thnk)
+;
+; Run a thunk in a new thread.  The thread will be given a random
+; label and all 'say' calls inside the thread will be prefixed with
+; this label.
 (define/contract (threaded thnk)
   (-> (-> any) any)
 
   (define thread-label (~a (rand-val "thread") ": "))
-  (parameterize ((prefix-for-say thread-label))
+  (parameterize ((prefix-for-say (~a (prefix-for-say) " " thread-label)))
     (thread (thunk (begin0 (thnk) (sleep 0)))))) ; the sleep 0 suggests other threads should run
