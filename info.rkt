232307bbfb93377921dd2422ab58671082602d3c
David K. Storrs <david.storrs@biomantica.com>
2018-03-06 23:35:52 -0500

    info.rkt: updated deps

--- info.rkt
+++ info.rkt
@@ -3,3 +3,11 @@
 (define collection "handy")
 (define version "1.0")
 (define test-omit-paths '("test-more.rkt"))
+
+(define deps '("base"
+               "db-lib"
+               "rackunit-lib"
+               "sxml"
+               ))
+(define build-deps '("at-exp-lib"
+                     ))
