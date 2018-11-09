613b6a2c018b06b559454b43722dae7b6ecb7032
David K. Storrs <david.storrs@biomantica.com>
2018-11-09 18:31:27 -0500

    finally put everything into 'main.rkt' so you can require all of handy at once if you want

--- main.rkt
+++ main.rkt
@@ -1,2 +1,35 @@
 #lang racket/base
 
+(require "HTML-Element.rkt"
+         "db.rkt"
+         "exceptions.rkt"
+         "files/compression.rkt"
+         "fsmonitor.rkt"
+         "json.rkt"
+         "list-utils.rkt"
+         "net.rkt"
+         "sql.rkt"
+         "struct.rkt"
+         "test-more.rkt"
+         "thread.rkt"
+         (except-in "utils.rkt" hash->keyword-apply); already required from list-utils.rkt
+         "web.rkt")
+
+(expect-n-tests 0)
+
+(provide (all-from-out  "HTML-Element.rkt")
+         (all-from-out  "db.rkt")
+         (all-from-out  "exceptions.rkt")
+         (all-from-out  "files/compression.rkt")
+         (all-from-out  "fsmonitor.rkt")
+         (all-from-out  "json.rkt")
+         (all-from-out  "list-utils.rkt")
+         (all-from-out  "net.rkt")
+         (all-from-out  "sql.rkt")
+         (all-from-out  "struct.rkt")
+         (all-from-out  "test-more.rkt")
+         (all-from-out  "thread.rkt")
+         (all-from-out  "utils.rkt")
+         (all-from-out  "web.rkt"))
+
+ 
